02753145d257349382169a7240c2dbca2d8ace52
unknown <kelvinnogithub@gmail.com>
2026-08-20 21:47:36 -0300

    Html puxa cursos do banco

--- core/views.py
+++ core/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Curso
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    curso = Curso.objects.all()
+
+    context = {
+        'curso': curso
+    }
+    return render(request, 'index.html', context)
